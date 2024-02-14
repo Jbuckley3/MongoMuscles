c4c666c806cafbc08af8116b4eb1a2b1f13eb8a7
Jbuckley <buckleyjosh3@gmail.com>
2024-02-14 10:24:23 -0500

    basic styling updates and one api call in home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,17 @@
+import { useState, useEffect } from "react"
+import { getAllExercises } from "../api/exercise"
+
 const Home = (props) => {
-	// const { msgAlert, user } = props
-	console.log('props in home', props)
+	const { msgAlert, user } = props
+	//	console.log('props in home', props)
+
+	const [exercises, setExercises] = useState(null)
+
+	useEffect(() => {
+		getAllExercises()
+			.then(res => console.log('pets: \n', res.data.exercises))
+			.catch(error => console.error)
+	}, [])
 
 	return (
 		<>
